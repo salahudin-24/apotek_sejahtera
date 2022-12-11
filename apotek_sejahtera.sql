f6f5cde4368dc23d951a08ff275d843ab406f7cb
Salahudin Al Ayubi <56055107+salahudin-24@users.noreply.github.com>
2022-12-11 14:39:34 +0700

    Added mysql database export

---
+++ apotek_sejahtera.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2022 at 08:38 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apotek_sejahtera`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cabang`
+--
+-- Error reading structure for table apotek_sejahtera.cabang: #1932 - Table 'apotek_sejahtera.cabang' doesn't exist in engine
+-- Error reading data for table apotek_sejahtera.cabang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `apotek_sejahtera`.`cabang`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+-- Error reading structure for table apotek_sejahtera.obat: #1932 - Table 'apotek_sejahtera.obat' doesn't exist in engine
+-- Error reading data for table apotek_sejahtera.obat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `apotek_sejahtera`.`obat`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan`
+--
+-- Error reading structure for table apotek_sejahtera.pemesanan: #1932 - Table 'apotek_sejahtera.pemesanan' doesn't exist in engine
+-- Error reading data for table apotek_sejahtera.pemesanan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `apotek_sejahtera`.`pemesanan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table apotek_sejahtera.users: #1932 - Table 'apotek_sejahtera.users' doesn't exist in engine
+-- Error reading data for table apotek_sejahtera.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `apotek_sejahtera`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
